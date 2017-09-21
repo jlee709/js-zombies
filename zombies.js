77c309322e59fffe0d902d500ca3c1069dc874ec
Justin Lee <leejwg.3@gmail.com>
2017-09-20 16:05:40 -1000

    equipped with

--- zombies.js
+++ zombies.js
@@ -110,16 +110,13 @@ Player.prototype.useItem = function(item){
   }
 };
 
-
- // * If the item is a weapon, the player should equip the item.
- // * If the item is food, the player should eat the item.
- // * You should be able to invoke this function on a Player instance.
- // *
- // * @name useItem
- // * @param {Item/Weapon/Food} item   The item to use.
- // */
-
-
+Player.prototype.equippedWith = function(){
+    if (this.equipped){
+      return this.equipped.name;
+    } else{
+      return false;
+    }
+  };
 /**
  * Player Class Method => equippedWith()
  * -----------------------------
